fff1c14cca029cc9d6750c6d9bc32be660947ab6
Juan Camilo Osorio <jcoc611@gmail.com>
2015-04-03 01:04:43 -0400

    Major update for v0.1.0.

---
+++ InputAffix.0.1.0.js
@@ -0,0 +1,187 @@
+// Created by Juan Camilo Osorio (JCOC611 - jcoc611.com).
+// Version 0.1.0. (Beta).
+// Consider giving back by sharing your own code!
+// Licensed under the MIT License. 
+// http://www.opensource.org/licenses/mit-license.php
+
+// Includes jQuery Caret v1.02
+// Slightly modified by JCOC611.
+// Created by C. F., Wong, and also licensed
+// under the MIT License. Find his project here:
+// https://code.google.com/p/jcaret/ 
+
+(function($) {
+	if(!$) throw new Error("Input Affix: no jQuery found. Make sure window.jQuery exists.");
+	// Functions
+	// Caret from Wong's code.
+	$.fn.caret = function(options, opt2) {
+		var start, end, t = this[0];
+		// For type aliasing
+		if (typeof options === "object" && typeof options.start === "number" && typeof options.end === "number") {
+			start = options.start;
+			end = options.end;
+		} else if (typeof options === "number" && typeof opt2 === "number") {
+			start = options;
+			end = opt2;
+		} else if (typeof options === "string") {
+			if ((start = t.value.indexOf(options)) > -1) end = start + options[len];
+			else start = null;
+		} else if (Object.prototype.toString.call(options) === "[object RegExp]") {
+			var re = options.exec(t.value);
+			if (re != null) {
+				start = re.index;
+				end = start + re[0][len];
+			}
+		}
+
+		// Alias for .caret(...) called with new selection information.
+		if (typeof start != "undefined") {
+			if (t.createTextRange) {
+				var selRange = t.createTextRange();
+				selRange.collapse(true);
+				selRange.moveStart('character', start);
+				selRange.moveEnd('character', end - start);
+				selRange.select();
+			} else {
+				t.selectionStart = start;
+				t.selectionEnd = end;
+			}
+			t.focus();
+			return this
+		// Alias for .caret(), with no arguments.
+		} else {
+			// Modification as suggested by Андрей Юткин
+			if(document.selection){
+				var selection = document.selection;
+				if (t.tagName.toLowerCase() != "textarea") {
+					var val = this.val(),
+						range = selection[createRange]()[duplicate]();
+					range.moveEnd("character", val[len]);
+					var s = (range.text == "" ? val[len] : val.lastIndexOf(range.text));
+					range = selection[createRange]()[duplicate]();
+					range.moveStart("character", -val[len]);
+					var e = range.text[len];
+				} else {
+					var range = selection[createRange](),
+						stored_range = range[duplicate]();
+					stored_range.moveToElementText(t);
+					stored_range.setEndPoint('EndToEnd', range);
+					var s = stored_range.text[len] - range.text[len],
+						e = s + range.text[len]
+				}
+				// End of Modification
+			} else {
+				var s = t.selectionStart,
+					e = t.selectionEnd;
+			}
+			var te = t.value.substring(s, e);
+			return {
+				start: s,
+				end: e,
+				text: te,
+				replace: function(st) {
+					return t.value.substring(0, s) + st + t.value.substring(e, t.value[len])
+				}
+			}
+		}
+	}
+	$.fn.prefix = function(pre){
+		if(!pre){
+			return this.data("prefix");
+		}
+		this.data("prefix", pre);
+		if(this.val().indexOf(pre) != 0){
+			this.val(pre + this.val());
+		}
+
+		this.on("keypress", function(e){
+			var caret = $(this).caret(),
+				prefix = $(this).data("prefix");
+			if(caret.start < prefix.length){
+				var end = caret.end;
+				if(end < prefix.length) end += prefix.length;
+				$(this).caret({start:prefix.length, end:end});
+			}
+		}).on("keydown", function(e){
+			var caret = $(this).caret(),
+				prefix = $(this).data("prefix");
+			// Backspace & delete fix 
+			if(e.which == 8 || e.which == 46){
+				if(e.which == 8 && caret.start <= prefix.length && caret.start == caret.end) e.preventDefault();
+				else if(caret.start < prefix.length && caret.start == caret.end) e.preventDefault();
+				else if(caret.start < prefix.length && caret.end == prefix.length){
+					$(this).caret({start:prefix.length, end:Math.max(caret.end, prefix.length)});
+					e.preventDefault();
+				}else if(caret.start < prefix.length){
+					$(this).caret({start:prefix.length, end:Math.max(caret.end, prefix.length)});
+				}
+			// Paste fix
+			}else if(e.which == 86 && e.ctrlKey){
+				if(caret.start < prefix.length){
+					var end = caret.end;
+					if(end < prefix.length) end += prefix.length;
+					$(this).caret({start:prefix.length, end:end});
+				}
+			// Home/start fix
+			}else if(e.which == 36){
+				$(this).caret({start:prefix.length, end:prefix.length});
+				e.preventDefault();
+			}
+
+		});
+		return this;
+	}
+	$.fn.suffix = function(suff){
+		// Overload for .suffix(), called with no arguments
+		if(!suff) return this.data("suffix");
+
+		// Else assume .suffix(str)
+		this.data("suffix", suff);
+		if(this.val().indexOf(suff, this.val().length - suff.length) == -1){
+			this.val(this.val() + suff);
+		}
+
+		// Event handlers
+		this.on("keypress", function(e){
+			var caret = $(this).caret(),
+				val = $(this).val(),
+				suffix = $(this).data("suffix");
+			if(caret.end > val.length - suffix.length){
+				var start = caret.start;
+				if(start > val.length - suffix.length) start -= suffix.length;
+				$(this).caret({start: start, end: val.length - suffix.length});
+			}
+		}).on("keydown", function(e){
+			var caret = $(this).caret(),
+				val = $(this).val(),
+				suffix = $(this).data("suffix");
+			// Backspace & delete fix 
+			if(e.which == 8 || e.which == 46){
+				if(e.which == 46 && caret.end >= val.length - suffix.length && caret.start == caret.end){
+					$(this).caret({start: val.length - suffix.length, end: val.length - suffix.length});
+					e.preventDefault();
+				}else if(e.which == 46 && caret.end >= val.length - suffix.length && caret.start == caret.end){
+					e.preventDefault();
+				}else if(caret.start > val.length - suffix.length && caret.start == val.length - suffix.length){
+					$(this).caret({start:val.length - suffix.length, end:val.length - suffix.length});
+					e.preventDefault();
+				}else if(caret.end > val.length - suffix.length){
+					$(this).caret({end:val.length - suffix.length, start:Math.min(caret.start, val.length - suffix.length)});
+				}
+			// Paste fix
+			}else if(e.which == 86 && e.ctrlKey){
+				if(caret.end > val.length - suffix.length){
+					var start = caret.start;
+					if(start > val.length -  suffix.length) start -= suffix.length;
+					$(this).caret({start:start, end:val.length - suffix.length});
+				}
+			// End fix
+			}else if(e.which == 35){
+				$(this).caret({start:val.length - suffix.length, end:val.length - suffix.length});
+				e.preventDefault();
+			}
+
+		});
+		return this;
+	}
+})(jQuery);
--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 ![Input Affix Logo](http://static.jcoc611.com/lab/InputAffix/logo.png)
 
+*Note: this plugin requires jQuery version 1.11 or above.*
+
 Input Affix is a jQuery plugin that allows you to add prefixes and suffixes to your input fields such that when your users type, the suffix or prefix does not change or move, and the user selection is managed accordingly. For example, if you need to add a percentage or degree symbol to one of your fields, it can easily be done by:
 
 ```javascript
@@ -20,3 +22,16 @@ Adds the prefix *pre* to the input field given by the jQuery selector. The user 
 
 ### .suffix(suff)
 Adds the suffix *suff* to the input field given by the jQuery selector. The user will be able to select/copy the suffix, but won't be able to delete it or write over it. Any text input will be inserted before the suffix, such that the suff string is kept at the end of the input at all times.
+
+*Note: calling both functions for an input field should also work.*
+
+## Hosted library
+In addition to the GitHub repo, I will also be hosting the minified version of the library at
+
+    http://static.jcoc611.com/hosted/js/InputAffix.0.1.0.min.js
+
+## Collaborating
+If you have any issues with the code, or have found any bugs, submit an Issue Ticket! Also, if you want to improve/change anything just fork the repo, make your changes, and submit a pull request. I will review it as soon as possible.
+
+Keep affixing your life with greatness,
+JCOC611.
